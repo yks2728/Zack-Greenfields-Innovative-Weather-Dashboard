42b30ba15e262d9749bfd028e520b0b0b7d023ec
Zack Greenfield <zackg007@verizon.net>
2022-01-05 15:13:54 -0500

    added five day containers and current container

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,32 @@
 var key = '6843ce9c306c263f9b4534c69341093d';
 var selectedCities = [];
-var cityInputEl = document.querySelector("#city-input");
+var cityInputEl = document.getElementById("city");
+var submitButton = document.getElementById("btn");
+var currentCity = document.getElementById("current-city");
+var currentTemperature = document.getElementById("current-temperature");
+var currentHumidity = document.getElementById("current-humidity");
+var currentUVIndex = document.getElementById("current-UV-index");
+
+function getUserRepos(city) {
+    console.log("getUserRepos");
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6843ce9c306c263f9b4534c69341093d&units=imperial")
+        .then(response=> response.json())
+        .then(data=> {
+            console.log(data);
+            currentCity.textContent = data.name
+            currentTemperature.textContent = data.main.temp
+            currentHumidity.textContent = data.main.humidity
+        })
+}  
+
+function searchForm (event) {
+    event.preventDefault();
+
+    var inputSearch = cityInputEl.value.trim();
+        console.log(inputSearch);
+        getUserRepos(inputSearch)
+}
+
+
+submitButton.addEventListener("click", searchForm);
+
