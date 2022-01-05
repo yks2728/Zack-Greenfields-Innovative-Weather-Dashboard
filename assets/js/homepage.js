0c04587772e0adb1f9370432d3f68bebccfb183e
Zack Greenfield <zackg007@verizon.net>
2022-01-05 13:56:17 -0500

    added selected cities string

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,3 @@
 var key = '6843ce9c306c263f9b4534c69341093d';
 var selectedCities = [];
 var cityInputEl = document.querySelector("#city-input");
-
-
-document.getElementById("input").onclick = function () {
-    var cityInput = document.getElementById("city-input").value;
-}
